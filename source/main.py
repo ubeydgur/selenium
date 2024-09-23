6f6819be349f63bc3de72524e19699a99096e072
Ubeyd Gür <ubeydgur41@gmail.com>
2024-09-24 00:33:21 +0300

    add information of the track being played, database operations and multithreading

--- source/main.py
+++ source/main.py
@@ -1,18 +1,37 @@
 from selenium.webdriver import Chrome
 from selenium.webdriver.common.by import By
+from collections import namedtuple
+from threading import Thread
 from time import sleep
+import csv
 
 
 bandcamp_url = "https://bandcamp.com/"
 
+TrackRec = namedtuple('TrackRec', [
+    'title',
+    'artist',
+    'artist_url',
+    'album',
+    'album_url'
+])
+
 class BandLeader():
-    def __init__(self):
+    def __init__(self, csvpath=None):
         self.driver = Chrome()
         self.driver.get(bandcamp_url)
 
-        self.current_track_number = 1
+        self.database_path = csvpath
+        self.database = []
+        self._current_track_record = None
+
+        self._current_track_number = 1
         self.track_list = []
-        self.tracks()
+
+        self.thread = Thread(target=self._maintain)
+        self.thread.daemon = True
+        self.thread.start()
+
 
     def tracks(self):
         sleep(1)
@@ -32,26 +51,75 @@ class BandLeader():
             print(f"Artist : {lines[1]}")
             print(f"Genre  : {lines[2]}\n")
         
-    def more_tracks(self, click='next'):
+
+    def more_tracks(self, click_button='next'):
         sleep(1)
 
-        hey = self.driver.find_elements(By.CLASS_NAME, 'item-page')
-        next_button = [e for e in hey
-                       if e.text.lower() == click]
+        items = self.driver.find_elements(By.CLASS_NAME, 'item-page')
+        next_button = [item for item in items
+                       if item.text.lower() == click_button]
         
         if next_button:
             next_button[0].click()
-            self.tracks()
+
 
     def play(self, track=None):
         if track is None:
-                self.driver.find_element(By.CLASS_NAME, "playbutton").click()
+            self.driver.find_element(By.CLASS_NAME, "playbutton").click()
         
         elif type(track) is int and track <= len(self.track_list) and track >= 1:
-                self.current_track_number = track
-                self.track_list[self.current_track_number - 1].click()
+            self.current_track_number = track
+            self.track_list[self.current_track_number - 1].click()
         
-        sleep(10)
+        sleep(1)
+
+        if self.is_playing():
+            self._current_track_record = self.currently_playing()   
+
 
     def pause(self):
-         self.play()
+        self.play()
+
+
+    def is_playing(self):
+        play_button = self.driver.find_element(By.CLASS_NAME, 'playbutton')
+
+        return play_button.get_attribute('class').find("playing") > -1
+    
+
+    def currently_playing(self):
+        if self.is_playing():
+            title = self.driver.find_element(By.CLASS_NAME, 'title').text
+            album_detail = self.driver.find_element(By.CSS_SELECTOR, ".detail-album > a")
+            album_title = album_detail.text
+            album_url = album_detail.get_attribute('href')
+            artist_detail = self.driver.find_element(By.CSS_SELECTOR, ".detail-artist > a")
+            artist_name = artist_detail.text
+            artist_url = artist_detail.get_attribute('href')
+
+            return TrackRec(title, artist_name, artist_url, album_title, album_url) 
+    
+
+    def _maintain(self):
+        while True:
+            self._update_db()
+            sleep(20)
+
+    
+    def _update_db(self):
+        check = (self._current_track_record is not None 
+                and (self.database[-1] != self._current_track_record 
+                     or len(self.database) == 0)
+                and self.is_playing())
+        
+        if check:
+            self.database.append(self._current_track_record)
+
+    
+    def save_db(self):
+        with open(self.database_path, 'w', newline='') as dbfile:
+            dbwriter = csv.writer(dbfile)
+            dbwriter.writerow(list(TrackRec._fields))
+
+            for entry in self.database:
+                dbwriter.writerow(list(entry))
